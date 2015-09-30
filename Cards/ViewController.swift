20dd2ae511f931ea73ab6b443bafb2c80153969f
Camo <camo@skout.com>
2015-09-30 16:00:12 -0300

    Initial Commit

---
+++ Cards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cards
+//
+//  Created by Camilo Vera Bezmalinovic on 9/30/15.
+//  Copyright © 2015 Camilo Vera Bezmalinovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
