369edc505f84fbc53c8265263a463b8a324a2a31
Camo <camo@skout.com>
2015-10-23 02:09:16 -0300

    gitignore

---
+++ Cards/CardCell.swift
@@ -0,0 +1,9 @@
+//
+//  CardCell.swift
+//  Cards
+//
+//  Created by Camilo Vera Bezmalinovic on 10/22/15.
+//  Copyright © 2015 Camilo Vera Bezmalinovic. All rights reserved.
+//
+
+import Foundation
---
+++ Cards/CardsLayout.swift
@@ -0,0 +1,9 @@
+//
+//  CardsLayout.swift
+//  Cards
+//
+//  Created by Camilo Vera Bezmalinovic on 9/30/15.
+//  Copyright © 2015 Camilo Vera Bezmalinovic. All rights reserved.
+//
+
+import Foundation
